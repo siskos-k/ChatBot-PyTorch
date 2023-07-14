2330f15461ebbc0bf6da723a6a0ac1607cd701e5
siskos-k <konstantinos.siscos@gmail.com>
2023-07-14 22:20:01 +0300

    e-shop implementation

---
+++ train.py
@@ -0,0 +1,137 @@
+import json
+import numpy as np
+from nltk_utils import tokenize, stem, bag_of_words
+
+from model import NeuralNet
+
+import torch
+import torch.nn as nn
+
+from torch.utils.data import Dataset, DataLoader
+
+with open('intents.json', 'r') as f:
+    intents = json.load(f)
+
+all_words = []
+tags = []
+xy = []
+
+for intent in intents['intents']:
+    tag = intent['tag']
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        w = tokenize(pattern)
+        all_words.extend(w)
+        xy.append((w, tag))
+#collecting is done
+
+ignore_words = ['?', '.', '!', ',']
+
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
+all_words = sorted(set(all_words))
+# convert to set to remove duplicates
+tags = sorted(set(tags))
+
+
+
+X_train = [] #bag of words
+Y_train = [] #tags
+
+for (pattern_sentence, tag) in xy:
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+
+    label = tags.index(tag)
+    Y_train.append(label) #cross loss??
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+
+    def __getitem__(self, idx):
+        return self.x_data[idx], self.y_data[idx]
+    
+
+    def __len__(self):
+        return self.n_samples
+    
+batch_size = 8
+hidden_size = 8
+learning_rate = 0.001
+num_epochs = 1000
+output_size = len(tags)
+input_size = len(all_words) #or len(X_train[0])
+
+
+class ChatDataset(Dataset):
+
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = Y_train
+
+    # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
