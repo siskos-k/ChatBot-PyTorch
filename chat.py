d83c39ae8de2f2a36ce86a0eeaf964a141394214
siskos-k <konstantinos.siscos@gmail.com>
2023-07-25 13:29:57 +0200

    functional front-end, base model

--- chat.py
+++ chat.py
@@ -1,11 +1,7 @@
-#implementing the actual chatting
-
 import random
 import json
-import torch
-from model import NeuralNet
-from nltk_utils import tokenize, bag_of_words, stem
 
+import torch
 
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
@@ -29,15 +25,10 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Makis"
-print("What's up loski (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
+bot_name = "Sam"
 
-    sentence = tokenize(sentence)
+def get_response(message):
+    sentence = tokenize(message)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -52,6 +43,18 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
-    else:
-        print(f"{bot_name}: I do not understand...")
+                return random.choice(intent['responses'])
+    
+    return "I do not understand..."
+
+
+if __name__ == "__main__":
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        # sentence = "do you use credit cards?"
+        sentence = input("You: ")
+        if sentence == "quit":
+            break
+
+        resp = get_response(sentence)
+        print(resp)
